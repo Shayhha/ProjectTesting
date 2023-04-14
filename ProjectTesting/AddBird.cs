515aeb4043600a10b473c1f59681ea33ca52d395
Shay Hahiashvili <shayhh@outlook.com>
2023-04-14 06:08:07 +0300

    ok

--- ProjectTesting/AddBird.cs
+++ ProjectTesting/AddBird.cs
@@ -328,27 +328,45 @@ namespace ProjectTesting
         public static void SortExcel(string name) //name can be "bird" or "cage"
         {
             int size=1; //default value
+            int colStart = 7;  //default value
+            int colEnd = 15; //default vaule
+            int flag = 0;
             Excel ex = new Excel("database", MainWindow.UserSheet);
             if (name == "bird")
-                size =ex.GetLastRow(7);
-            else if (name == "cage")
-                size = ex.GetLastRow(1);
-
-            string[][] arr = new string[size-1][]; //initalize the double array //size is -1 because of our default line in Shay!
-            int index = 0; //index for array
-            for (int i = 2; i < size; i++)
             {
-                string[] temp = ex.ReadRange(i, 7, 15);
-                arr[index] = temp;
-                index++;//increment index 
+                size = ex.GetLastRow(7);
+                colStart = 7;
+                colEnd = 15;
             }
-            //now we have an arr with all birds inside, not sorted
-            //here we call Sort method 
-            index = 0;
-            for(int j = 2; j < size; j++) //now we going through the database and update the birds list 
+            else if (name == "cage")
             {
-                ex.WriteRange(j, 7, 15, arr[index]);
-                index++;
+                size = ex.GetLastRow(1);
+                colStart = 1;
+                colEnd = 5;
+            }
+            else //means we got invalid string for operation, we do nothing
+            {
+                flag = 1;
+            }
+
+            if (flag == 0)
+            {
+                string[][] arr = new string[size - 1][]; //initalize the double array //size is -1 because of our default line in Shay!
+                int index = 0; //index for array
+                for (int i = 2; i < size; i++)
+                {
+                    string[] temp = ex.ReadRange(i, colStart, colEnd);
+                    arr[index] = temp;
+                    index++;//increment index 
+                }
+                //now we have an arr with all birds inside, not sorted
+                //here we call Sort method 
+                index = 0;
+                for (int j = 2; j < size; j++) //now we going through the database and update the birds list 
+                {
+                    ex.WriteRange(j, colStart, colEnd, arr[index]);
+                    index++;
+                }
             }
             ex.Quit();//close excel
         }
