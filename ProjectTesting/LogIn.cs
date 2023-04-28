2d1cebdb77ceb3c5fe81cc7ffe41c3ef5fce00b3
Shay Hahiashvili <shayhh@outlook.com>
2023-04-28 17:16:20 +0300

    b

--- ProjectTesting/LogIn.cs
+++ ProjectTesting/LogIn.cs
@@ -83,23 +83,21 @@ namespace ProjectTesting
         private void LoggedIn() //opens the homepage
         {
             Excel ex = new Excel("database", MainWindow.UserSheet);
-            if (ex.ReadCell("A1") == "")
+            //set bird and cage counters and top bar
+            ((MainWindow)this.Parent.Parent).setCagesLabel((ex.GetLastRow() - 1).ToString());
+            ((MainWindow)this.Parent.Parent).setBirdsLabel((ex.GetLastRow(7) - 1).ToString());
+            ((MainWindow)this.Parent.Parent).showTopBar();
+            ((MainWindow)this.Parent.Parent).showTopPanel();
+
+            if (ex.ReadCell("A1") == "") //checks if user is new (without any cages)
             {
                 ((MainWindow)this.Parent.Parent).addCage1.setWelcome_lable(true);
                 ((MainWindow)this.Parent.Parent).addCage1.Show();
             }
-            else
+            else //else user isn't new so we show homepage
             {
                 ((MainWindow)this.Parent.Parent).homePage1.Show();
             }
-
-
-            ((MainWindow)this.Parent.Parent).setCagesLabel((ex.GetLastRow() - 1).ToString());
-            ((MainWindow)this.Parent.Parent).setBirdsLabel((ex.GetLastRow(7) - 1).ToString());
-            
-            ((MainWindow)this.Parent.Parent).showTopBar();
-            ((MainWindow)this.Parent.Parent).showTopPanel();
-
             ex.Quit();//closes the database excel
             UserName_textbox.Text = "";
             Password_textbox.Text = "";
