c5c40d6d67da8b666128255d6fd2e0f95b14e445
Maxim Subotin <maxim.sub21@gmail.com>
2023-05-10 02:42:54 -0700

    adding new controls from an open source project

--- ProjectTesting/Properties/Resources.Designer.cs
+++ ProjectTesting/Properties/Resources.Designer.cs
@@ -59,5 +59,29 @@ namespace ProjectTesting.Properties {
                 resourceCulture = value;
             }
         }
+
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap calendarDark
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("calendarDark", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap calendarWhite
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("calendarWhite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
