2b106ffe1e91b072ba0891ba174488fb1bdb7551
Shay Hahiashvili <shayhh@outlook.com>
2023-05-11 20:44:26 +0300

    ff

--- ProjectTesting/AddBird.cs
+++ ProjectTesting/AddBird.cs
@@ -307,8 +307,7 @@ namespace ProjectTesting
                                 MainWindow.HashTable.ClearBirdCageHashtable();//clear the hashtables of bird and cage
                                 MainWindow.InitHashtable(); //calling initHashtable for bird and cage hashtables
                             }
-                            else if (currentBirdRow != 1 && int.Parse(LogIn.DataBaseExcel.ReadCell("G" + (currentBirdRow - 1))) > int.Parse(birdInfo[0])
-                            || int.Parse(LogIn.DataBaseExcel.ReadCell("G" + (currentBirdRow + 1))) < int.Parse(birdInfo[0]))
+                            else if (currentBirdRow != 1 && int.Parse(LogIn.DataBaseExcel.ReadCell("G" + (currentBirdRow - 1))) > int.Parse(birdInfo[0]))
                             {
                                 MainWindow.SortExcel("bird");//calls SortExcel from MainWindow
                                 MainWindow.HashTable.ClearBirdCageHashtable();//clear the hashtables of bird and cage
