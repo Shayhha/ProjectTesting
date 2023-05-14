cf911e5093217273906279f45ef5c6274a6bb186
Shay Hahiashvili <shayhh@outlook.com>
2023-05-14 14:43:47 +0300

    fixed log in

--- ProjectTesting/LogIn.cs
+++ ProjectTesting/LogIn.cs
@@ -24,18 +24,22 @@ namespace ProjectTesting
         {
             string username = UserName_textbox.Text;
             string password = Password_textbox.Text;
-            string[] info = { username, password };
             List<string[]> result = MainWindow.HashTable.SearchUserHashtable(username);
-            if (result[0][0] == username && result[0][1] != password)
+
+            if (username == "" && password == "") //if fields are empty we show error
+            {
+                CustomMessageBox.Show("Fields are empty!", "Input Error");
+            }
+            else if (result[0][0] == username && result[0][1] != password) // if password as invalid we show error
             {
                 CustomMessageBox.Show("Wrong password!", "Password Error");
             }
-            else if (result[0][0] == username && result[0][1] == password)
+            else if (result[0][0] == username && result[0][1] == password) //if user wrote valid information we call LoggedIn method
             {
                 MainWindow.UserSheet = username; //gives usersheet the current users name
                 LoggedIn();
             }
-            else
+            else //else no user found and we show error 
             {
                 CustomMessageBox.Show("No matching user \"" + username + "\"", "User Error");
             }
