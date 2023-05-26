65388d3b2b0786e222c9456e5a41832f85da1684
Maxim Subotin <maximsu@ac.sce.ac.il>
2023-05-26 10:38:27 -0700

    adding comments

--- ProjectTesting/Cage.cs
+++ ProjectTesting/Cage.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 
 namespace ProjectTesting
 {
+    /// <summary>
+    /// This class represents a bird in our program, it has all the necessary parameters and methods 
+    /// </summary>
     public class Cage
     {
         // Fields
@@ -15,10 +13,10 @@ namespace ProjectTesting
         public string Length { get; set; }
         public string Width { get; set; }
         public string Height { get; set; }
-        public List<Bird> BirdList { get; set; }
+        public List<Bird> BirdList { get; set; } // a list of birds that are in the cage
 
         // Constructor
-        public Cage(string[] cageInfo)
+        public Cage(string[] cageInfo) // cageInfo is a string array that holds all of the important info about the cage
         {
             Id = cageInfo[0];
             Length = cageInfo[1];
@@ -26,7 +24,7 @@ namespace ProjectTesting
             Height = cageInfo[3];
             Material = cageInfo[4];
 
-            BirdList = new List<Bird>();
+            BirdList = new List<Bird>(); // when creating a new cage it doesnt have birds
         }
 
         public Cage(Cage otherCage)
@@ -37,10 +35,10 @@ namespace ProjectTesting
             Width = otherCage.Width;
             Height = otherCage.Height;
 
-            BirdList = otherCage.BirdList.Select(b => new Bird(b)).ToList();
+            BirdList = otherCage.BirdList.Select(b => new Bird(b)).ToList(); //copys the list to current object
         }
 
-        public void AddBird(Bird bird)
+        public void AddBird(Bird bird) // adding an bird into the list of birds of the cage
         {
             BirdList.Add(bird);
         }
@@ -50,13 +48,13 @@ namespace ProjectTesting
             BirdList = list;
         }
 
-        public string[] ToStringArray() //string array representation of class
+        public string[] ToStringArray() //turns the cage into a string array representation
         {
             string[] cageInfo = new string[5] { this.Id, this.Length, this.Width, this.Height, this.Material };
             return cageInfo;
         }
 
-        public void EditFields(string[] cageInfo)
+        public void EditFields(string[] cageInfo) // allows us to edit the cage info
         {
             this.Id = cageInfo[0];
             this.Length = cageInfo[1];
