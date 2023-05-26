ef85e8f34ace6ca7516882573e1544536c94e836
Shay Hahiashvili <shayhh@outlook.com>
2023-05-26 20:19:53 +0300

    ff

--- ProjectTesting/AddCage.cs
+++ ProjectTesting/AddCage.cs
@@ -13,7 +13,6 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ProjectTesting
 {
